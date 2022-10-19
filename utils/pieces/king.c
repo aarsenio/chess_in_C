dff32e3a6cafa7c34576131fb5b79237aadc738a
Alexandre Arsenio ribeiro alves <aarsenio@c2r4s3.42lisboa.com>
2022-10-19 18:35:16 +0100

    add king movement

--- utils/pieces/king.c
+++ utils/pieces/king.c
@@ -1,5 +1,21 @@
 #include "../../headers/chess.h"
 
+static int check_move(int x, int y)
+{
+	int	math_x,math_y;
+	if (data()->map[x][y] && data()->map[x][y]->player == data()->select->player)
+		return (0);
+	math_x = x - data()->select->x;
+	math_y = y - data()->select->y;
+	if (math_x < 0)
+		math_x *= -1;
+	if (math_y < 0)
+		math_y *= -1;
+	if (math_x > 1 || math_y > 1)
+		return (0);
+	return (1);
+}
+
 t_piece *new_king(int player, int x, int y)
 {
 	t_piece *piece = new_piece(player, x, y);
@@ -9,6 +25,7 @@ t_piece *new_king(int player, int x, int y)
 		load_img("imgs/king2.xpm", &piece->img);
 	piece->type = KING;
 	piece->is_move = 0;
+	piece->check_move = check_move;
 	data()->map[x][y] = piece;
 	return piece;
 }
